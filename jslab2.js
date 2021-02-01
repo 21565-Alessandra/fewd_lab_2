e2c65d3fb209fc51dbcbe549657ab90c1f4b5cf4
21565-Alessandra <71334857+21565-Alessandra@users.noreply.github.com>
2021-02-01 23:05:17 +0000

    Update jslab2.js

--- jslab2.js
+++ jslab2.js
@@ -1,7 +1,10 @@
 //Alessandra Silva dos Reis - Student ID: 21565
 
+
+// Questions 3 and 4
 var myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G'];
         console.log(myAlphabet.length);
+
     
 // Questions 5 and 6
 const Planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Saturn'];
@@ -23,3 +26,53 @@ var hello = function(){
     return 'Hello World!';
     };
     console.log(hello());
+
+/*
+QUESTION 8
+var x = 5, y = 10, z = 15;
+console.log(x + y); = 15
+console.log(y – x); = 5
+console.log(x * y); = 50
+console.log(y / x); = 2
+console.log(x % 2); = 1
+console.log(x++); = 6
+console.log(x--); = 4
+*/
+
+/*
+QUESTION 9
+var a = 5, b = "Hello", c = "World", d = 10;
+
+a + b = 5Hello
+b + c = HelloWorld
+a + d = 15
+*/
+
+/*
+QUESTION 10
+var a = 5, b = 10, c = "5";
+var x = a;
+
+console.log(a == c); //true
+console.log(a === c); //false
+console.log(a == x); //true
+console.log(a != b); //true
+console.log(a > b); //false
+console.log(a < b); //true
+console.log(a >= b); //false
+console.log(a <= b); //true
+console.log(a >= c); //true
+console.log(a <= c); //true   
+*/
+
+/*
+QUESTION 13
+var hello = function(){
+    return 'Hi';
+};
+
+var x = hello();
+console.log(x)
+*/
+
+
