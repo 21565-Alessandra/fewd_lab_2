237193605f9a99555b075565aa3a96e8bc5bad33
21565-Alessandra <71334857+21565-Alessandra@users.noreply.github.com>
2021-02-01 22:43:17 +0000

    Update jslab2.js

--- jslab2.js
+++ jslab2.js
@@ -3,22 +3,22 @@
 var myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G'];
         console.log(myAlphabet.length);
     
-//5 and 6
-const Planets = ['Earth', 'Mercury', 'Venus', 'Mars', 'Jupiter', 'Uranus'];
+// Questions 5 and 6
+const Planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Saturn'];
 console.log(Planets[0]);
 console.log(Planets[1]);
 console.log(Planets[2]);
 console.log(Planets[3]);
 console.log(Planets[4]);
 
-// 7
-const wowDatatypes = [3, 'home', false];
-var x = wowDatatypes[0];
-var y = wowDatatypes[1];
-var z = wowDatatypes[2];
+// Question 7
+const wowDataTypes = [true, 'cat', 7];
+var x = wowDataTypes[0];
+var y = wowDataTypes[1];
+var z = wowDataTypes[2];
 console.log(x,y,z);
 
-//11 and 12
+//Questions 11 and 12
 var hello = function(){
     return 'Hello World!';
     };
